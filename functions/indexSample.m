b38f6dbd91a8de180ed990612ff20f835da49528
术生何 <urfdvw@gmail.com>
2019-04-08 16:14:16 -0400

    better index sample funciton

--- functions/indexSample.m
+++ functions/indexSample.m
@@ -1,5 +1,12 @@
 function [ind] = indexSample(sample_n, W)
 % Sample the index by the 
+W(isnan(W)) = 0;
 M = length(W);
-ind = randsample(M,sample_n,true,W);
+try
+    ind = randsample(M,sample_n,true,W);
+catch
+    max(W)
+    W
+    error('randsample error')
+end
 end
