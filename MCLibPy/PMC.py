a865f397a1e14db6205836d80b0a11346af32079
urfdvw <urfdvw@gmail.com>
2019-05-27 17:06:14 -0400

    PMC HD banana example

--- MCLibPy/PMC.py
+++ MCLibPy/PMC.py
@@ -1,22 +1,25 @@
 import functions as fn
 import numpy as np
 from scipy.stats import multivariate_normal as mvn
+from tqdm import tqdm
+import matplotlib.pyplot as plt
+
 
 class PMC:
     '''
     Population Monte Carlo class
-    
+
     The class only record particles from the last sampling step.
     History particles are not recorded inside the class.
     This means the shape of particle group is not changed.
     To get samples from multiple sampling steps,
     recording is needed outside the class.
     '''
-    
+
     def __init__(self, mu0, K, logtarget):
         '''
         construction funciton
-        
+
         inputs:
             mu0: N*D, nparray
             K: scaler, int
@@ -30,11 +33,11 @@ class PMC:
             resample_method: 'global' or 'local'
             x: N*K*D nparray: samples
             w: N*K nparray: sample weights
-            
+
             logp = logtarget(x) : lambda: log target distribution
                 x: M*D, nparray
                 logp: M, nparray
-                
+
         '''
         # passing the parameters
         self.mu = mu0
@@ -46,78 +49,99 @@ class PMC:
         self.rho = 1.0
         self.resample_method = 'global'
         # initial sampling
-        self.x = np.zeros(shape=(self.N,self.K,self.D)) # space allocation
-        self.w = np.zeros(shape=(self.N,self.K)) # space allocation
+        self.x = np.zeros(shape=(self.N, self.K, self.D))  # space allocation
+        self.w = np.zeros(shape=(self.N, self.K))  # space allocation
         for n in range(self.N):
-            self.x[n,:,:] = mvn.rvs(mean=self.mu[n,:],
-                                  cov=self.C,
-                                  size=self.K)
-            self.w[n,:] = np.ones(shape=self.K)/self.K
+            self.x[n, :, :] = mvn.rvs(
+                    mean=self.mu[n, :],
+                    cov=self.C,
+                    size=self.K)
+            self.w[n, :] = np.ones(shape=self.K) / self.K
         return
+
     def setSigma(self, sig):
         '''
         set the proposal covariance by the std of each dimension
-        
+
         sig: std of each dimension
         '''
         self.C = np.eye(self.D)*(sig**2)
         return
-    def setTemp(self, rho):
+
+    def setRho(self, rho):
         '''
         set target distribution rempering
         '''
         self.rho = rho
         return
+
     def sample(self):
         '''
         main procesdure
         '''
-        logw_n = np.ones([self.N,self.K]) # log weights of samples for current population
-        logTw_n = np.ones([self.N,self.K]) # log tempered weights of samples for current population
-        for n in range(self.N): # for each population
-            logprop = np.ones([self.K]) # log proposal probability
-            for k in range(self.K): # for each particle in the population
-                self.x[n,k,:] = mvn.rvs(mean=self.mu[n,:],cov=self.C,size=1) # sampling from proposal
-                logprop[k] = fn.logmean(mvn.logpdf(x=self.mu, mean=self.x[n,k,:], cov=self.C)) # DM-weights
-            logw_n[n,:] = self.logtarget(self.x[n,:,:]) - logprop # weights
-            logTw_n[n,:] = self.logtarget(self.x[n,:,:])**self.rho - logprop # tempered witghts
+        # log weights of samples for current population
+        logw_n = np.ones([self.N, self.K])
+        # log tempered weights of samples for current population
+        logTw_n = np.ones([self.N, self.K])
+        for n in range(self.N):  # for each population
+            logprop = np.ones([self.K])  # log proposal probability
+            for k in range(self.K):  # for each particle in the population
+                self.x[n, k, :] = mvn.rvs(
+                        mean=self.mu[n, :],
+                        cov=self.C,
+                        size=1)  # sampling from proposal
+                logprop[k] = fn.logmean(
+                        mvn.logpdf(
+                                x=self.mu,
+                                mean=self.x[n, k, :],
+                                cov=self.C))  # DM-weights
+            # weights
+            logw_n[n, :] = self.logtarget(self.x[n, :, :]) - logprop
+            # tempered witghts
+            logTw_n[n, :] = self.logtarget(self.x[n, :, :])*self.rho - logprop
         # prepare global particles for output
-        outx = np.reshape(self.x,(-1,self.D))
-        outlogw = np.reshape(logw_n,(-1))
-        outlogTw = np.reshape(logTw_n,(-1))
+        outx = np.reshape(self.x, (-1, self.D))
+        outlogw = np.reshape(logw_n, (-1))
+        outlogTw = np.reshape(logTw_n, (-1))
         # resampling
         if self.resample_method == 'global':
-            ind = np.random.choice(a=np.arange(self.N*self.K),p=fn.logw2w(outlogTw),size=self.N)
-            self.mu = outx[ind,:]
+            ind = np.random.choice(
+                    a=np.arange(self.N*self.K),
+                    p=fn.logw2w(outlogTw),
+                    size=self.N)
+            self.mu = outx[ind, :]
         elif self.resample_method == 'local':
             for n in range(self.N):
-                ind = np.random.choice(a=np.arange(self.K),p=fn.logw2w(logTw_n[n,:]))
-                self.mu[n,:] = self.x[n,ind,:]
+                ind = np.random.choice(
+                        a=np.arange(self.K),
+                        p=fn.logw2w(logTw_n[n, :]))
+                self.mu[n, :] = self.x[n, ind, :]
         else:
             print('wrong resample type')
         return outx, outlogw
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     # example
-    def lognormal(x,D):
-        '''
-        Target distribution
-        
-        x: M*D, nparray
-        logp: M, nparray
-        '''
-        logp = mvn.logpdf(x, mean=10*np.ones(shape=D),cov=np.eye(D))
-        return logp
-    
-    D = 10 # number of dimension of sampling space
-    N = 30 # number of particles per population
-    K = 50 # number of populations
+
+    D = 4  # number of dimension of sampling space
+    N = 50  # number of particles per population
+    K = 50  # number of populations
+    M = 50  # number of iterations
     mu0 = mvn.rvs(mean=np.zeros(shape=D),
-                cov=np.eye(D) * 3,
-                size=N) # initial mean of each population
-    logtarget = lambda x: lognormal(x,D)
-    pmc = PMC(mu0,K,logtarget) # define pmc object
+                  cov=np.eye(D) * 3,
+                  size=N)  # initial mean of each population
+    logtarget = lambda x: fn.logbanana(x, D)
+    pmc = PMC(mu0, K, logtarget)  # define pmc object
     pmc.resample_method = 'local'
-    for i in range(20):    
+
+    sig_plan = np.linspace(3, 0.1, num=M)
+    rho_plan = np.logspace(-3, 0, num=M)
+    plt.figure()
+    for i in tqdm(range(M)):
+        pmc.setSigma(sig_plan[i])
+        pmc.setRho(rho_plan[i])
         outx, outlogw = pmc.sample()
-        print(fn.weightedsum(outx, fn.logw2w(outlogw)))
+        plt.clf()
+        fn.plotsamples(outx, fn.logw2w(outlogw))
+        plt.pause(0.1)
--- MCLibPy/functions.py
+++ MCLibPy/functions.py
@@ -1,9 +1,15 @@
 import numpy as np
+import matplotlib.pyplot as plt
+from scipy.stats import multivariate_normal as mvn
+'''
+Sample related
+'''
+
 
 def logw2w(logw):
     '''
     convert log weightd to normalized weights
-    
+
     logw: nparray vector
     '''
     logw = logw - np.max(logw)
@@ -11,11 +17,12 @@ def logw2w(logw):
     sumw = np.sum(w)
     w = w / sumw
     return w
-    
+
+
 def logmean(logw):
     '''
     return the log of mean of weights given the log weights
-    
+
     logw: nparray vector
     return: float
     '''
@@ -28,16 +35,49 @@ def logmean(logw):
     m = np.mean(w)
     return np.log(m) + log_scale
 
-def weightedsum(x,w):
+
+def weightedsum(x, w):
     '''
     Weighted sum of vectors
-    
+
     x: M*D, nparray
     w: M, nparray
-    
+
     accu: D, nparray
     '''
-    accu = np.ones(np.size(x,1))*0
+    accu = np.ones(np.size(x, 1))*0
     for i in range(len(w)):
-        accu += w[i]*x[i,:]
+        accu += w[i] * x[i, :]
     return accu
+
+
+def plotsamples(x, w):
+    alpha = w / np.max(w) * 0.2
+    color = np.array([0.8, 0.2, 0.6])
+    rgba = np.zeros((len(w), 4))
+    for i in range(len(w)):
+        rgba[i, 0:3] = color
+        rgba[i, 3] = alpha[i]
+    plt.scatter(x[:, 0], x[:, 1], c=rgba, marker='o', s=100)
+
+
+'''
+Target Distributions
+'''
+
+
+def logbanana(x, D):
+    p = 0
+    for d in range(D-1):
+        p += 100*(x[:, d+1]-x[:, d]**2)**2 + (1-x[:, d])**2
+    return -p
+
+def lognormal(x, D):
+    '''
+    Target distribution
+
+    x: M*D, nparray
+    logp: M, nparray
+    '''
+    logp = mvn.logpdf(x, mean=10*np.ones(shape=D), cov=np.eye(D))
+    return logp
