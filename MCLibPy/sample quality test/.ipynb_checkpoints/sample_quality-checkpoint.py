a2a11efb8da91ab54882100c9c2a33bf696dccf0
术生何 <urfdvw@gmail.com>
2019-05-28 13:02:22 -0400

    distance experiments

--- MCLibPy/sample quality test/.ipynb_checkpoints/sample_quality-checkpoint.py
+++ MCLibPy/sample quality test/.ipynb_checkpoints/sample_quality-checkpoint.py
@@ -1,4 +1,4 @@
-"""
+'''
 funcitons that measure the sample qualities, sometimes with respect to a target distribution
 
 input:
@@ -12,32 +12,28 @@ input:
 
 output:
     out: float: the measured quality
-"""
+'''
 
 import numpy as np
 
-
 def ESS(w):
     # effective sample size
-    return 1 / np.sum(w ** 2)
-
+    return 1/np.sum(w**2)
 
 def ESSoverN(w):
     # effective sample size over N
     # return value will between 0 and 1
     N = len(w)
-    return ESS(w) / N
-
+    return ESS(w)/N
 
 def Dchi2(x, pi, w=None):
     if not w:
-        N = np.size(x, 0)
-        w = np.ones(N) / N
-    return np.sum(w ** 2 / pi(x))
-
+        N = np.size(x,0)
+        w = np.ones(N)/N
+    return np.sum(w**2/pi(x))
 
 def Dchi2N(x, pi, w=None):
     if not w:
-        N = np.size(x, 0)
-        w = np.ones(N) / N
-    return np.sum(w ** 2 / pi(x)) * N
+        N = np.size(x,0)
+        w = np.ones(N)/N
+    return np.sum(w**2/pi(x)) * N
