a117bbf2797ce4461f21bfce9b9df803c7a9ce2f
术生何 <urfdvw@gmail.com>
2019-06-17 16:48:30 -0400

    GauMix test

--- MCLibPy/TEST_GauMix.py
+++ MCLibPy/TEST_GauMix.py
@@ -13,50 +13,32 @@ import matplotlib.pyplot as plt
 import PMC
 import PMCxVAE
 
-def TwoDlogbanana(x):
-    B = 10
-    ng1 = 4
-    ng2 = 3.5
-    ng3 = 3.5
-    p = 1/(2*ng1*ng1)\
-        *(4 - B*x[:, 0] - x[:, 1]*x[:, 1])\
-        *(4 - B*x[:, 0] - x[:, 1]*x[:, 1])\
-        +x[:, 0]*x[:, 0]/(2*ng2*ng2)\
-        +x[:, 1]*x[:, 1]/(2*ng3*ng3)
-    return -p
+def GauMix(x):
+    D = 10
+    Lambda = np.eye(D) * 3
+    v1 = np.ones(D) * 6
+    v2 = np.ones(D) * (-5)
+    v3 = np.array([1, 2, 3, 4, 5, 5, 4, 3, 2, 1])
+    p1 = mvn.pdf(x, mean=v1, cov=Lambda)
+    p2 = mvn.pdf(x, mean=v2, cov=Lambda)
+    p3 = mvn.pdf(x, mean=v3, cov=Lambda)
+    return np.log((p1 + p2 + p3)/3)
 
-#%% fine grid
-N = 100
-maxx = 10
-minx = -10
-
-x = np.linspace(minx, maxx, N)
-w = np.zeros([N, N])
-y = np.zeros([N*N, 2])
-
-iy = 0
-for i in range(N):
-    for j in range(N):
-        x_now = np.zeros([1, 2])
-        x_now[0, 0] = x[i]
-        x_now[0, 1] = x[j]
-        y[iy, :] = x_now
-        iy += 1
-
-logw = TwoDlogbanana(y)
-w = fn.logw2w(logw)
-
-print(fn.weightedsum(y, w))
+ExTheory = np.array([
+        2/3, 1, 4/3,
+        5/3, 2, 2,
+        5/3, 4/3, 1, 2/3
+        ])
 
 #%% PMC
-D = 2  # number of dimension of sampling space
+D = 10  # number of dimension of sampling space
 N = 50  # number of particles per population
 K = 20  # number of populations
-M = 20  # number of iterations
+M = 100  # number of iterations
 mu0 = mvn.rvs(mean=np.zeros(shape=D),
               cov=np.eye(D) * 3,
               size=N)  # initial mean of each population
-pmc = PMC.PMC(mu0, K, TwoDlogbanana)  # define pmc object
+pmc = PMC.PMC(mu0, K, GauMix)  # define pmc object
 pmc.resample_method = 'local'
 
 sig_plan = np.linspace(2, 0.1, num=M)
@@ -67,7 +49,7 @@ logw = np.zeros((0))
 plt.figure()
 for i in tqdm(range(M)):
     pmc.setSigma(sig_plan[i])
-#        pmc.setRho(rho_plan[i])
+    pmc.setRho(rho_plan[i])
     outx, outlogw = pmc.sample()
     x = np.concatenate((x, outx))
     logw = np.concatenate((logw, outlogw))
@@ -77,20 +59,19 @@ for i in tqdm(range(M)):
 plt.clf()
 fn.plotsamples(x, fn.logw2w(logw))
 plt.show()
-error = fn.weightedsum(x, fn.logw2w(logw)) - np.array([-0.4845, 0])
+error = fn.weightedsum(x, fn.logw2w(logw)) - ExTheory
 print(error)
 print(np.sqrt(np.sum(error**2)))
 
 #%% VAExPMC
-Dh = 2
-Dz = 2
-N = 1000  # number of particles per population
-M = 20  # number of AIS iterations
+Dh = 6
+Dz = 3
+N = N * K  # number of particles per population
 mu0 = mvn.rvs(mean=np.zeros(shape=D),
               cov=np.eye(D) * 3,
               size=N)  # initial mean of each population
 
-pmc = PMCxVAE.PMC(mu0, TwoDlogbanana, Dz, Dh)  # define pmc object
+pmc = PMCxVAE.PMC(mu0, GauMix, Dz, Dh)  # define pmc object
 plt.figure()
 
 rhoxz_plan = np.linspace(1.2, 1, num=M)
@@ -111,6 +92,6 @@ for i in range(M):
 plt.clf()
 fn.plotsamples(x, fn.logw2w(logw))
 plt.show
-error = fn.weightedsum(x, fn.logw2w(logw)) - np.array([-0.4845, 0])
+error = fn.weightedsum(x, fn.logw2w(logw)) - ExTheory
 print(error)
 print(np.sqrt(np.sum(error**2)))
